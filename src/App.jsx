a9f06921751fbee7f7fea2ebfe6f945e6c79d9e4
Hashir <hashirshah222@gmail.com>
2023-09-03 18:40:19 -0500

    Removed Contact page

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
                                 <Route exact path='/blog' element={<BlogsPage />} />
                                 <Route exact path='/about' element={<AboutPage />} />
                                 <Route exact path='/booknotes' element={<BookNotesPage />} />
-                                <Route exact path='/contact' element={<ContactPage />} />
+                                {/* <Route exact path='/contact' element={<ContactPage />} /> */}
                                 <Route exact path='/error' element={<ErrorPage />} />
                                 <Route exact path='/blog/:id' element={<SingleBlogPage />} />
 
